5c41b4126850da00628bddf0a3c07f980b94d11d
AKuchko <vasco_da_gama1469@mail.ru>
2025-05-11 02:09:33 +0700

    fix: App, add main logic

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,111 @@
 <script setup lang="ts">
+import { ref, useTemplateRef } from 'vue'
+import { GameState } from './types';
+import { useGameStore } from './stores/GameStore';
+import GamePowerScale from './components/HitPowerScale.vue';
+import GameButton from './components/GameControlButton.vue';
+import GameRobot from './components/GameRobot.vue';
+import HitAnimation from './components/HitAnimation.vue';
+import ScoreScale from './components/ScoreScale.vue';
 
+const gameStore = useGameStore()
+
+const scoreLevel = ref(0)
+
+const scoreEl = useTemplateRef<InstanceType<typeof ScoreScale>>('scoreEl')
+
+const startGame = () => {
+  gameStore.setGameState(GameState.InGame)
+  gameStore.setHitPower(0)
+  scoreLevel.value = 0
+  scoreEl.value?.resetScore()
+}
+
+const makeHit = () => {
+  gameStore.setGameState(GameState.Hitted)
+}
+
+const onHitAnimationEnd = () => {
+  scoreLevel.value = Math.round(gameStore.hitPower / (100 / gameStore.levelsCount));
+  console.log('onHitAnimationEnd', scoreLevel.value);
+
+  scoreEl.value?.startScoreAnimation(scoreLevel.value)
+}
+
+const onScoreAnimationEnd = () => {
+  if (scoreLevel.value !== gameStore.levelsCount) {
+    gameStore.setGameState(GameState.Loser)
+  } else {
+    gameStore.setGameState(GameState.Winner)
+  }
+}
 </script>
 
 <template>
-  <main>
-    <h1>Testing</h1>
+  <main class="app">
+    <div class="app__wrapper">
+      <ScoreScale
+        ref="scoreEl"
+        @animationend="onScoreAnimationEnd"
+      />
+      <HitAnimation
+        @animationend="onHitAnimationEnd"
+      />
+      <div class="app__control">
+        <GamePowerScale class="app__dynamometer" />
+        <GameButton
+          class="app__play-button"
+          @new-game="startGame"
+          @make-hit="makeHit"
+        />
+        <GameRobot class="app__robot-view" />
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.app {
+  width: 100%;
+  height: 100%;
+  padding: 30px 13px;
+  background-image: url('@/assets/images/bg.webp');
+  background-size: auto 640px;
+  background-repeat: no-repeat;
+  background-position: 50% 44px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.app__wrapper {
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+  max-width: 360px;
+  max-height: 640px;
+
+  padding-bottom: 107px;
+
+  margin: auto;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.app__control {
+  position: absolute;
+  bottom: 0;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-column-gap: 11px;
+  align-items: end;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  width: 100%;
+}
+
+.app__dynamometer {
+  margin: 0 14px;
 }
 </style>
