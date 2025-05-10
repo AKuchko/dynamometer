a7359dc73651edb50a8ca920d2ef5cdde74384a3
AKuchko <vasco_da_gama1469@mail.ru>
2025-05-11 02:07:50 +0700

    feat: Hammer hit animation

---
+++ src/components/HitAnimation.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue';
+import { useGameStore } from '@/stores/GameStore';
+import { GameState, type CSSClasses } from '@/types';
+import HammerImage from '@/assets/images/hammer.webp'
+import HitButton from '@/components/icons/HitButton.vue';
+
+const emit = defineEmits<{
+  (e: 'animationend'): void;
+}>()
+
+const gameStore = useGameStore()
+
+const isAnimationFinished = ref(false)
+
+const hammerEl = ref<HTMLElement | null>(null)
+
+const startHitAnimation = () => {
+  if (!hammerEl.value) return
+
+  const animationFrames: Keyframe[] = [
+    { transform: 'rotate(0deg) translate(-5px, 0)' },
+    { transform: 'rotate(45deg) translate(10px, -20px)' },
+    { transform: 'rotate(-90deg) translate(55px, -31.5px)' },
+  ]
+  const animationOptions: KeyframeAnimationOptions = { duration: 600, iterations: 1 };
+  const animation = hammerEl.value.animate(animationFrames, animationOptions)
+  animation.onfinish = onAnimationEnd;
+}
+
+const resetHitAnimation = () => {
+  isAnimationFinished.value = false
+}
+
+const onAnimationEnd = () => {
+  isAnimationFinished.value = true
+  emit('animationend')
+}
+
+watch(() => gameStore.state, () => {
+  if (gameStore.state === GameState.Hitted) startHitAnimation()
+  if (gameStore.state === GameState.InGame) resetHitAnimation()
+})
+
+const rootCssClasses = computed((): CSSClasses => ({
+  'hit-animation--ready': gameStore.state === GameState.InGame,
+  'hit-animation--finished': isAnimationFinished.value,
+}))
+</script>
+
+<template>
+  <div
+    class="hit-animation"
+    :class="rootCssClasses"
+  >
+    <div class="hit-animation__wrapper">
+      <img
+        :src="HammerImage"
+        class="hit-animation__hammer"
+        ref="hammerEl"
+      >
+      <HitButton :pushed="isAnimationFinished" />
+    </div>
+  </div>
+</template>
+
+<style>
+.hit-animation {
+  min-height: 68px;
+}
+
+.hit-animation__wrapper {
+  position: relative;
+  display: inline-block;
+}
+
+.hit-animation__hammer {
+  position: absolute;
+  left: 100%;
+  top: -80%;
+
+  width: 90px;
+
+  transition: all 0.6s ease;
+  transform: rotate(-45deg);
+  transform-origin: bottom center;
+}
+
+.hit-animation--ready .hit-animation__hammer {
+  transform: rotate(0) translateX(-5px);
+}
+
+.hit-animation--finished .hit-animation__hammer {
+  transition: none;
+  transform: rotate(-90deg) translate(55px, -31.5px);
+}
+</style>
---
+++ src/components/icons/HitButton.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const { pushed } = defineProps<{
+  pushed?: boolean;
+}>()
+
+const currentViewBox = computed(() => {
+  return pushed
+    ? '0 0 124 60'
+    : '0 0 124 64'
+})
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" :viewBox="currentViewBox" width="124">
+    <template v-if="pushed">
+      <path fill="url(#a)" d="M18 0h88l18 35H0L18 0Z"/>
+      <path fill="url(#b)" d="M0 35h124v25H0V35Z"/>
+      <ellipse cx="62.31" cy="18.25" fill="#C53030" rx="33.31" ry="8.99"/>
+      <ellipse cx="62.31" cy="17.99" fill="#F65A5A" rx="33.31" ry="8.99"/>
+      <ellipse cx="62.31" cy="17" fill="url(#d)" fill-opacity=".4" rx="32" ry="8"/>
+    </template>
+    <template v-else>
+      <path fill="url(#a)" d="M18 8h88l18 35H0L18 8Z"/>
+      <path fill="url(#b)" d="M0 43h124v25H0V43Z"/>
+      <path fill="#DE1D1D" fill-rule="evenodd" d="M95.63 8.92H29v17.85h.06c1 4.72 15.5 8.47 33.25 8.47s32.26-3.75 33.26-8.47h.06V8.92Z" clip-rule="evenodd"/>
+      <path fill="url(#c)" fill-rule="evenodd" d="M95.63 8.92H29v17.85h.06c1 4.72 15.5 8.47 33.25 8.47s32.26-3.75 33.26-8.47h.06V8.92Z" clip-rule="evenodd"/>
+      <ellipse cx="62.31" cy="8.99" fill="#F65A5A" rx="33.31" ry="8.99"/>
+      <ellipse cx="62.31" cy="8" fill="url(#d)" fill-opacity=".4" rx="32" ry="8"/>
+    </template>
+    <defs>
+      <linearGradient id="b" x1="62" x2="62" y1="68" y2="34.82" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#73C4E7"/>
+        <stop offset="1" stop-color="#141893"/>
+      </linearGradient>
+      <linearGradient id="c" x1="62.31" x2="62.31" y1="8.92" y2="35.24" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#D90000"/>
+        <stop offset="1" stop-color="#A60000"/>
+      </linearGradient>
+      <linearGradient id="d" x1="32.31" x2="68.31" y1="6.5" y2="6.5" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#fff"/>
+        <stop offset="1" stop-color="#fff" stop-opacity="0"/>
+      </linearGradient>
+      <radialGradient id="a" cx="0" cy="0" r="1" gradientTransform="matrix(0 17.5 -62 0 62 25.5)" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#6C99D1"/>
+        <stop offset="1" stop-color="#142563"/>
+      </radialGradient>
+    </defs>
+  </svg>
+</template>
